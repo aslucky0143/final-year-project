caa095cefc42c6fb5ee102d63c68485e680a582d
Sai Lucky <aslucky0143@gmail.com>
2024-03-28 20:07:33 +0530

    addition of GUI

---
+++ XGBoostmodel.py
@@ -0,0 +1,66 @@
+# %load_ext google.colab.data_table
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#@title Default title text
+df = pd.read_csv('cirrhosis.csv', index_col='ID')
+print(df.head(2))
+# For Numerical Type
+df.select_dtypes(include=(['int64', 'float64'])).isna().sum()
+df.select_dtypes(include=(['int64', 'float64'])).isna().sum()
+df_num_col = df.select_dtypes(include=(['int64', 'float64'])).columns
+for c in df_num_col:
+    df[c].fillna(df[c].median(), inplace=True)
+
+df.select_dtypes(include=(['int64', 'float64'])).isna().sum()
+# For Categorical type
+df.select_dtypes(include=('object')).isna().sum()
+df_cat_col = df.select_dtypes(include=('object')).columns
+for c in df_cat_col:
+    df[c].fillna(df[c].mode().values[0], inplace=True)
+print(df) 
+
+#print(df.select_dtypes(include=('object')).isna().sum())
+df['Stage'] = np.where(df['Stage'] == 4,1,0)
+# replacing catagorical data with intigers.
+df['Sex'] = df['Sex'].replace({'M':0, 'F':1})                                # Male : 0 , Female :1
+df['Ascites'] = df['Ascites'].replace({'N':0, 'Y':1})                        # N : 0, Y : 1   
+df['Drug'] = df['Drug'].replace({'D-penicillamine':0, 'Placebo':1})          # D-penicillamine : 0, Placebo : 1   
+df['Hepatomegaly'] = df['Hepatomegaly'].replace({'N':0, 'Y':1})              # N : 0, Y : 1
+df['Spiders'] = df['Spiders'].replace({'N':0, 'Y':1})                        # N : 0, Y : 1
+df['Edema'] = df['Edema'].replace({'N':0, 'Y':1, 'S':-1})                    # N : 0, Y : 1, S : -1
+df['Status'] = df['Status'].replace({'C':0, 'CL':1, 'D':-1})                 # 'C':0, 'CL':1, 'D':-1
+# Setting up Features and Target
+X = df.drop(['Status', 'N_Days', 'Stage'], axis=1)
+y = df.pop('Stage')
+print(df.columns)
+print("y",y)
+from sklearn.model_selection import StratifiedKFold
+from xgboost import XGBClassifier
+skf = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+model = XGBClassifier(learning_rate=0.75, max_depth=3, random_state=1, gamma=0, eval_metric='error') # tried learning rate values between range [0.01 - 10] & depth [2-8]
+acc=[]
+def training(train, test, fold_no):
+  X_train = train
+  y_train = y.iloc[train_index]
+  X_test = test
+  y_test = y.iloc[test_index]
+  model.fit(X_train, y_train)
+  score = model.score(X_test,y_test)
+  acc.append(score)
+  #print('For Fold {} the accuracy is {}'.format(str(fold_no),score))
+fold_no = 1
+for train_index,test_index in skf.split(X, y):
+  train = X.iloc[train_index,:]
+  test = X.iloc[test_index,:]
+  training(train, test, fold_no)
+  fold_no += 1
+print('XGboost model Mean Accuracy = ', np.mean(acc))
+
+from sklearn.metrics import classification_report
+XGB_model_predict = model.predict(test)
+XGB_model_predict_proba = model.predict_proba(test)
+
+def predict(user):
+  return model.predict(user)
---
+++ main.py
@@ -0,0 +1,128 @@
+import tkinter as tk
+from tkinter import messagebox
+import XGBoostmodel
+import pandas as pd
+def predict_user_data():
+    # Create a DataFrame with user input
+    data = {}
+    for field, entry in entries.items():
+        data[field] = entry.get()
+    user = pd.DataFrame({
+        'Drug' :[float(drug_var.get())],
+        'Age':[float(data['Age'])],
+        'Sex':[float(gender_var.get())],
+        'Ascites':[float(ascites_var.get())],
+        'Hepatomegaly' :[float(hepatomegaly_var.get())],
+        'Spiders':[float(spiders_var.get())],
+        'Edema':[float(edema_var.get())],
+        'Bilirubin':[float(data['Bilirubin'])],
+        'Cholesterol':[float(data['Cholesterol'])],
+        'Albumin':[float(data['Albumin'])],
+        'Copper':[float(data['Copper'])],
+        'Alk_Phos':[float(data['Alk_Phos'])],
+        'SGOT':[float(data['SGOT'])],
+        'Tryglicerides':[float(data['Tryglicerides'])],
+        'Platelets':[float(data['Platelets'])],
+        'Prothrombin':[float(data['Prothrombin'])],
+    })
+    
+    print(user.to_string())
+    """user['Sex'] = user['Sex'].replace({'M':0, 'F':1})                                # Male : 0 , Female :1
+    user['Ascites'] = user['Ascites'].replace({'N':0, 'Y':1})                        # N : 0, Y : 1   
+    user['Drug'] = user['Drug'].replace({'D-penicillamine':0, 'Placebo':1})          # D-penicillamine : 0, Placebo : 1   
+    user['Hepatomegaly'] = user['Hepatomegaly'].replace({'N':0, 'Y':1})              # N : 0, Y : 1
+    user['Spiders'] = user['Spiders'].replace({'N':0, 'Y':1})                        # N : 0, Y : 1
+    user['Edema'] = user['Edema'].replace({'N':0, 'Y':1, 'S':-1})                    # N : 0, Y : 1, S : -1
+    user['Status'] = user['Status'].replace({'C':0, 'CL':1, 'D':-1})                 # 'C':0, 'CL':1, 'D':-1
+    """ # Predict using the trained XGBoost model
+    prediction = XGBoostmodel.predict(user)
+    if prediction==[1]:
+        prediction="Cirrihosis Confirmed"
+    else:
+        prediction="No Cirrihosis "
+    messagebox.showinfo("Prediction", prediction)
+    
+
+
+def get_status():
+    status = status_var.get()
+    messagebox.showinfo("Selected Status", f"You selected: {status}")
+
+# Create main application window
+root = tk.Tk()
+root.title("Categorical Data Selection")
+
+# Create a variable to hold the selected options
+gender_var = tk.IntVar()
+ascites_var = tk.IntVar()
+drug_var = tk.IntVar()
+hepatomegaly_var = tk.IntVar()
+spiders_var = tk.IntVar()
+edema_var = tk.IntVar()
+status_var = tk.IntVar()
+
+# Create radio buttons for selecting gender
+male_radio = tk.Radiobutton(root, text="Male (0)", variable=gender_var, value=0, )
+female_radio = tk.Radiobutton(root, text="Female (1)", variable=gender_var, value=1, )
+
+# Create radio buttons for selecting ascites
+no_ascites_radio = tk.Radiobutton(root, text="No Ascites (0)", variable=ascites_var, value=0, )
+yes_ascites_radio = tk.Radiobutton(root, text="Ascites (1)", variable=ascites_var, value=1, )
+
+# Create radio buttons for selecting drug
+penicillamine_radio = tk.Radiobutton(root, text="D-penicillamine (0)", variable=drug_var, value=0, )
+placebo_radio = tk.Radiobutton(root, text="Placebo (1)", variable=drug_var, value=1, )
+
+# Create radio buttons for selecting hepatomegaly
+no_hepatomegaly_radio = tk.Radiobutton(root, text="No Hepatomegaly (0)", variable=hepatomegaly_var, value=0, )
+yes_hepatomegaly_radio = tk.Radiobutton(root, text="Hepatomegaly (1)", variable=hepatomegaly_var, value=1, )
+
+# Create radio buttons for selecting spiders
+no_spiders_radio = tk.Radiobutton(root, text="No Spiders (0)", variable=spiders_var, value=0, )
+yes_spiders_radio = tk.Radiobutton(root, text="Spiders (1)", variable=spiders_var, value=1, )
+
+# Create radio buttons for selecting edema
+no_edema_radio = tk.Radiobutton(root, text="No Edema (0)", variable=edema_var, value=0, )
+edema_radio = tk.Radiobutton(root, text="Edema (1)", variable=edema_var, value=1, )
+resolved_edema_radio = tk.Radiobutton(root, text="Resolved Edema (-1)", variable=edema_var, value=-1, )
+
+# Place the radio buttons in the window
+tk.Label(root, text="Sex").grid(row=0,column=0)
+male_radio.grid(row=0,column=1)
+female_radio.grid(row=0,column=2)
+
+tk.Label(root, text="Ascites").grid(row=1,column=0)
+no_ascites_radio.grid(row=1,column=1)
+yes_ascites_radio.grid(row=1,column=2)
+
+tk.Label(root, text="Penicillamine").grid(row=2,column=0)
+penicillamine_radio.grid(row=2,column=1)
+placebo_radio.grid(row=2,column=2)
+tk.Label(root, text="Hepatomegaly").grid(row=3,column=0)
+no_hepatomegaly_radio.grid(row=3,column=1)
+yes_hepatomegaly_radio.grid(row=3,column=2)
+
+
+tk.Label(root, text="Spiders").grid(row=4,column=0)
+no_spiders_radio.grid(row=4,column=1)
+yes_spiders_radio.grid(row=4,column=2)
+
+tk.Label(root, text="Edema").grid(row=5,column=0)
+no_edema_radio.grid(row=5,column=1)
+edema_radio.grid(row=5,column=2)
+resolved_edema_radio.grid(row=5,column=3)
+
+entries = {}
+fields=["Age","Bilirubin","Cholesterol","Albumin","Copper","Alk_Phos","SGOT","Tryglicerides","Platelets","Prothrombin"]
+i=6
+for field in fields:
+    tk.Label(root, text=field).grid(row=i,column=0)
+    entry = tk.Entry(root)
+    entry.grid(row=i,column=1)
+    i+=1
+    entries[field] = entry
+    
+submit_button = tk.Button(root, text="Predict", command=predict_user_data)
+submit_button.grid(row=i+5, columnspan=2,)
+# Start the GUI event loop
+root.mainloop()
